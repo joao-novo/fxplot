5482a2587e632d8f03f235ad25e763256844a01c
João Novo <joaonovo05@protonmail.com>
2024-05-27 12:26:25 +0100

    feat: add request receiving and input handling

--- src/plotting/plotting.py
+++ src/plotting/plotting.py
@@ -1,5 +1,17 @@
+import requests
+import json
+
+url = "http://localhost:8080/"
+
 def main() -> None:
-    pass
+    function = input("Enter the desired function: ")
+    params = {
+        "function": function
+    }
+    
+    response = requests.get(url, params=params)
+    fn_result = response.json()
+    print(fn_result)
 
 if __name__ == "__main__":
     main()
