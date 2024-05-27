90e35440668a4fe3560ac3dcc95a8a62b7438457
João Novo <joaonovo05@protonmail.com>
2024-05-27 14:15:38 +0100

    feat: implemented function creating

--- src/plotting/plotting.py
+++ src/plotting/plotting.py
@@ -1,17 +1,31 @@
 import requests
 import json
+from typing import Callable
 
 url = "http://localhost:8080/"
 
-def main() -> None:
-    function = input("Enter the desired function: ")
-    params = {
-        "function": function
-    }
-    
-    response = requests.get(url, params=params)
-    fn_result = response.json()
-    print(fn_result)
+def create_function(coeffs: list[list[int]], signs: list[int]) -> Callable[float, float]: 
+	def func(x: float) -> float:
+		res = 0
+		for i, monomial in enumerate(coeffs):
+			if signs[i] == 43:
+				res += monomial[0] * x ** monomial[1] 
+			else:
+				res -= monomial[0] * x ** monomial[1]
+		return res
+	return func
 
+def main() -> None:
+	while True:
+		function = input("Enter the desired function: ")
+		params = {
+		    "function": function
+		}
+
+		response = requests.get(url, params=params)
+		fn_result = response.json()
+		func = create_function(fn_result["coeffs"], fn_result["signs"])
+		print(func(1))
+	
 if __name__ == "__main__":
-    main()
+	main()
