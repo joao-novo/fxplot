b0e74ea4c193df5ae28bb1a064c9567e5a29fc8e
João Novo <joaonovo05@protonmail.com>
2024-05-27 14:26:28 +0100

    feat: add function values generating

--- src/plotting/plotting.py
+++ src/plotting/plotting.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import numpy as np
+import numpy.typing as npt
 from typing import Callable
 
 url = "http://localhost:8080/"
@@ -8,24 +10,33 @@ def create_function(coeffs: list[list[int]], signs: list[int]) -> Callable[float
 	def func(x: float) -> float:
 		res = 0
 		for i, monomial in enumerate(coeffs):
-			if signs[i] == 43:
-				res += monomial[0] * x ** monomial[1] 
-			else:
-				res -= monomial[0] * x ** monomial[1]
+			match signs[i]:
+				case 43: # 43 == '+'
+					res += monomial[0] * x ** monomial[1] 
+				case 45: # 45 == '-'
+					res -= monomial[0] * x ** monomial[1]
+				case _:
+					continue
 		return res
 	return func
+
+def generate_values(func: Callable[float, float]) -> npt.NDArray[float]:
+	vals = np.linspace(-100, 100, 1000000)
+	return np.array([y for y in func(vals)])
+
+
 
 def main() -> None:
 	while True:
 		function = input("Enter the desired function: ")
 		params = {
-		    "function": function
+			"function": function
 		}
 
 		response = requests.get(url, params=params)
 		fn_result = response.json()
 		func = create_function(fn_result["coeffs"], fn_result["signs"])
-		print(func(1))
+
 	
 if __name__ == "__main__":
 	main()
