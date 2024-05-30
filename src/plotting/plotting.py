29ba1a4f62e3b84e51269cafd6bf9a992cdaab6d
João Novo <joaonovo05@protonmail.com>
2024-05-30 12:29:02 +0100

    added graphing

--- src/plotting/plotting.py
+++ src/plotting/plotting.py
@@ -1,12 +1,13 @@
 import requests
 import json
 import numpy as np
-import numpy.typing as npt
+import plotly.express as px
+import pandas as pd
 from typing import Callable
 
 url = "http://localhost:8080/"
 
-def create_function(coeffs: list[list[int]], signs: list[int]) -> Callable[float, float]: 
+def create_function(coeffs: list[list[int]], signs: list[int]) -> Callable[[float], float]: 
 	def func(x: float) -> float:
 		res = 0
 		for i, monomial in enumerate(coeffs):
@@ -20,22 +21,25 @@ def create_function(coeffs: list[list[int]], signs: list[int]) -> Callable[float
 		return res
 	return func
 
-def generate_values(func: Callable[float, float]) -> npt.NDArray[float]:
+def generate_values(func: Callable[[float], float]) -> pd.DataFrame:
 	vals = np.linspace(-100, 100, 1000000)
-	return np.array([y for y in func(vals)])
+	return pd.DataFrame([func(y) for y in vals])
 
 
 
 def main() -> None:
-	while True:
-		function = input("Enter the desired function: ")
-		params = {
-			"function": function
-		}
+	function = input("Enter the desired function: ")
+	params = {
+		"function": function
+	}
 
-		response = requests.get(url, params=params)
-		fn_result = response.json()
-		func = create_function(fn_result["coeffs"], fn_result["signs"])
+	response = requests.get(url, params=params)
+	fn_result = response.json()
+	func = create_function(fn_result["coeffs"], fn_result["signs"])
+	df = generate_values(func)
+	fig = px.line(df)
+	fig.write_html("../../fig.html")
+
 
 	
 if __name__ == "__main__":
